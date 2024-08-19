f6a812ba304d5f10da992d88e532f5d5829a3743
Asswanth Chaitanya Saradhi <88917196+codersaradhi@users.noreply.github.com>
2024-08-19 23:35:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function startingSadhana(){
     autoplay.innerHTML = `<div> <h1>pathanga asanam
     </h1>
     <audio   controls autoplay >
-        <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+        <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
 
 // sisupala asanam right
 setTimeout(function(){
     autoplay.innerHTML = `<div> <h1>sisupala asanam right
         </h1>
         <audio   controls autoplay >
-            <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+            <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
             
 },2*60*1000)// pathang asanam time
 
@@ -24,7 +24,7 @@ setTimeout(function(){
     autoplay.innerHTML = `<div> <h1>sisupala asanam left
         </h1>
         <audio   controls autoplay >
-            <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+            <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
             
 },4*60*1000)// sisupala asanam right
 
@@ -33,7 +33,7 @@ setTimeout(function(){
     autoplay.innerHTML = `<div> <h1>nadii-vibajana asanam 3 sets
         </h1>
         <audio   controls autoplay >
-            <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+            <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
             
 },6*60*1000)// sisupala asanam left
 
@@ -42,7 +42,7 @@ setTimeout(function(){
     autoplay.innerHTML = `<div> <h1>step 1 in shambavi asanam 
         </h1>
         <audio   controls autoplay >
-            <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+            <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
             
 },9*60*1000)//nadii-vibajana asanam 3 sets
 
@@ -51,7 +51,7 @@ setTimeout(function(){
     autoplay.innerHTML = `<div> <h1>step 2 in shambavi chant 21 times om
         </h1>
         <audio   controls autoplay >
-            <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+            <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
             
 },15*60*1000)//step 1 in shambavi 
 
@@ -60,7 +60,7 @@ setTimeout(function(){
     autoplay.innerHTML = `<div> <h1>step 3 in shambavi (viparitha swasaa)
         </h1>
         <audio   controls autoplay >
-            <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+            <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
             
 },18*60*1000)//step 2 in shambavi (21 times om)
 
@@ -69,7 +69,7 @@ setTimeout(function(){
     autoplay.innerHTML = `<div> <h1>step 4 in shambavi asanam
         </h1>
         <audio   controls autoplay >
-            <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+            <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
             
 },23*60*1000)//step 3 in shambavi (viparitha swasaa)
 
@@ -78,7 +78,7 @@ setTimeout(function(){
     autoplay.innerHTML = `<div> <h1>step 5 in shambavi asanam
         </h1>
         <audio   controls autoplay >
-            <source src="files/bell-172780.mp3" type="audio/mp3"></audio> </div>`
+            <source src="./bell-172780.mp3" type="audio/mp3"></audio> </div>`
             
 },25*60*1000)//step 4 in shambavi nadi bandham 
 
